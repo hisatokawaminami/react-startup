0e50409eea5ab7caad392c7ec987bf8497125bf7
Guest User <Guest@epicodus-41.local>
2018-09-10 12:17:50 -0700

    add lifting state

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function NewTicketForm(){
+function NewTicketForm(props){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
+
+  function handleNewTicketFormSubmission(event) {
+  event.preventDefault();
+  console.log(_names.value);
+  console.log(_location.value);
+  console.log(_issue.value);
+  props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value});
+  _names.value = '';
+  _location.value = '';
+  _issue.value = '';
+
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names' />
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) => {_location = input;}}/>
         <textarea
           id='issue'
           placeholder='Describe your issue.'
-        />
-        <button>Help!</button>
+          ref={(textarea) => {_issue = textarea;}}/>
+        <button type='submit'>Help!</button>
       </form>
     </div>
+
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
